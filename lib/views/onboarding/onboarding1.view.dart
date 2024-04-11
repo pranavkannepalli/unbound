a5dd4ceb8c2279daa5a6ab6db358b528261988b8
PranavK226 <pranav.kannepalli@gmail.com>
2024-04-11 10:41:32 -0700

    started onboarding

---
+++ lib/views/onboarding/onboarding1.view.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class Onboarding1 extends StatefulWidget {
+  const Onboarding1({super.key});
+
+  @override
+  State<Onboarding1> createState() => _Onboarding1State();
+}
+
+class _Onboarding1State extends State<Onboarding1> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
