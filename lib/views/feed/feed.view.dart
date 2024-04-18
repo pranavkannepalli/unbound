0a115bbc167e2d2af1845f7431a3795679441e73
Achintya72 <achintya.agrawal.90@gmail.com>
2024-04-17 20:59:41 -0700

    Fixed weird scrolling issues

--- lib/views/feed/feed.view.dart
+++ lib/views/feed/feed.view.dart
@@ -16,11 +16,14 @@ class FeedPage extends StatefulWidget {
 class _FeedState extends State<FeedPage> with SingleTickerProviderStateMixin {
   late TabController controller;
   late Future<List<Feed>?> future;
+  late ScrollController scrollController;
   int currentIndex = 0;
+  double scrollPos = 0;
 
   void handleTabChange() {
     setState(() {
       currentIndex = controller.index;
+      scrollPos = 0;
     });
   }
 
@@ -30,6 +33,12 @@ class _FeedState extends State<FeedPage> with SingleTickerProviderStateMixin {
     future = DatabaseService().getFeeds();
     controller = TabController(length: 3, vsync: this);
     controller.addListener(handleTabChange);
+    scrollController = ScrollController();
+    scrollController.addListener(() {
+      setState(() {
+        scrollPos = scrollController.position.pixels;
+      });
+    });
   }
 
   @override
@@ -48,129 +57,141 @@ class _FeedState extends State<FeedPage> with SingleTickerProviderStateMixin {
           Feed? studentFeed = (snapshot.data?[1]);
           Feed? internshipFeed = (snapshot.data?[2]);
           return SafeArea(
-            child: SingleChildScrollView(
-              child: Container(
-                height: MediaQuery.of(context).size.height,
-                width: double.infinity,
-                child: Column(children: [
-                  Container(
-                    margin: const EdgeInsets.all(20),
-                    child: TextField(
-                      decoration: textInputDecoration.copyWith(
-                          hintText: "Search",
-                          prefixIcon: Icon(Ionicons.search,
-                              size: 16, color: white.shade700)),
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    ),
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              width: double.infinity,
+              child: Column(children: [
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: white.shade50, 
+                    boxShadow: scrollPos > 0 ?  [BoxShadow(color: Color.fromRGBO(0, 0, 0, 0.25), blurRadius: 16)] : []
                   ),
-                  Container(
-                      width: double.infinity,
-                      padding: const EdgeInsets.all(2),
-                      margin: const EdgeInsets.symmetric(horizontal: 20),
-                      decoration: BoxDecoration(
-                          color: white.shade400,
-                          borderRadius: BorderRadius.circular(10)),
-                      child: TabBar(
-                          controller: controller,
-                          indicatorSize: TabBarIndicatorSize.tab,
-                          dividerColor: Colors.transparent,
-                          indicator: BoxDecoration(
-                              color: white.shade50,
-                              borderRadius: BorderRadius.circular(8),
-                              boxShadow: const [
-                                BoxShadow(
-                                    color: Color.fromRGBO(0, 0, 0, 0.25),
-                                    blurRadius: 8)
-                              ]),
-                          tabs: [
-                            Tab(
-                              height: 30,
-                              child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Icon(
-                                        currentIndex == 0
-                                            ? Ionicons.library
-                                            : Ionicons.library_outline,
-                                        size: 14,
-                                        color: currentIndex == 0
-                                            ? purple.shade400
-                                            : white.shade700),
-                                    const SizedBox(width: 8),
-                                    Text("Colleges",
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .bodySmall)
-                                  ]),
-                            ),
-                            Tab(
-                              height: 30,
-                              child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Icon(
-                                        currentIndex == 1
-                                            ? Ionicons.people
-                                            : Ionicons.people_outline,
-                                        size: 14,
-                                        color: currentIndex == 1
-                                            ? green.shade400
-                                            : white.shade700),
-                                    const SizedBox(width: 8),
-                                    Text("People",
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .bodySmall)
-                                  ]),
-                            ),
-                            Tab(
-                              height: 30,
-                              child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Icon(
-                                        currentIndex == 2
-                                            ? Ionicons.briefcase
-                                            : Ionicons.briefcase_outline,
-                                        size: 14,
-                                        color: currentIndex == 2
-                                            ? blue.shade600
-                                            : white.shade700),
-                                    const SizedBox(width: 8),
-                                    Text("Internships",
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .bodySmall)
-                                  ]),
-                            ),
-                          ])),
-                  Expanded(
-                      child: TabBarView(
-                    controller: controller,
+                  child: Column(
                     children: [
-                      if (collegeFeed != null)
-                        ListView.builder(
-                            itemCount: collegeFeed.posts.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              return PostWidget(post: collegeFeed.posts[index]);
-                            }),
-                      if (studentFeed != null)
-                        ListView.builder(
-                            itemCount: studentFeed.posts.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              return PostWidget(post: studentFeed.posts[index]);
-                            }),
-                      if (internshipFeed != null)
-                        ListView.builder(
-                            itemCount: internshipFeed.posts.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              return PostWidget(
-                                  post: internshipFeed.posts[index]);
-                            }),
+                      TextField(
+                        decoration: textInputDecoration.copyWith(
+                            hintText: "Search",
+                            prefixIcon: Icon(Ionicons.search,
+                                size: 16, color: white.shade700)),
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      const SizedBox(height: 12),
+                      Container(
+                          width: double.infinity,
+                          padding: const EdgeInsets.all(2),
+                          decoration: BoxDecoration(
+                              color: white.shade400,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: TabBar(
+                              controller: controller,
+                              indicatorSize: TabBarIndicatorSize.tab,
+                              dividerColor: Colors.transparent,
+                              indicator: BoxDecoration(
+                                  color: white.shade50,
+                                  borderRadius: BorderRadius.circular(8),
+                                  boxShadow: const [
+                                    BoxShadow(
+                                        color: Color.fromRGBO(0, 0, 0, 0.25),
+                                        blurRadius: 8)
+                                  ]),
+                              tabs: [
+                                Tab(
+                                  height: 30,
+                                  child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                            currentIndex == 0
+                                                ? Ionicons.library
+                                                : Ionicons.library_outline,
+                                            size: 14,
+                                            color: currentIndex == 0
+                                                ? purple.shade400
+                                                : white.shade700),
+                                        const SizedBox(width: 8),
+                                        Text("Colleges",
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodySmall)
+                                      ]),
+                                ),
+                                Tab(
+                                  height: 30,
+                                  child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                            currentIndex == 1
+                                                ? Ionicons.people
+                                                : Ionicons.people_outline,
+                                            size: 14,
+                                            color: currentIndex == 1
+                                                ? green.shade400
+                                                : white.shade700),
+                                        const SizedBox(width: 8),
+                                        Text("People",
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodySmall)
+                                      ]),
+                                ),
+                                Tab(
+                                  height: 30,
+                                  child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                            currentIndex == 2
+                                                ? Ionicons.briefcase
+                                                : Ionicons.briefcase_outline,
+                                            size: 14,
+                                            color: currentIndex == 2
+                                                ? blue.shade600
+                                                : white.shade700),
+                                        const SizedBox(width: 8),
+                                        Text("Internships",
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodySmall)
+                                      ]),
+                                ),
+                              ])),
                     ],
-                  ))
-                ]),
-              ),
+                  ),
+                ),
+                Expanded(
+                    child: TabBarView(
+                  controller: controller,
+                  children: [
+                    if (collegeFeed != null)
+                      ListView.builder(
+                          controller: scrollController,
+                          itemCount: collegeFeed.posts.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return PostWidget(post: collegeFeed.posts[index]);
+                          }),
+                    if (studentFeed != null)
+                      ListView.builder(
+                          controller: scrollController,
+                          itemCount: studentFeed.posts.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return PostWidget(post: studentFeed.posts[index]);
+                          }),
+                    if (internshipFeed != null)
+                      ListView.builder(
+                          controller: scrollController,
+                          itemCount: internshipFeed.posts.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return PostWidget(
+                                post: internshipFeed.posts[index]);
+                          }),
+                  ],
+                ))
+              ]),
             ),
           );
         } else {
