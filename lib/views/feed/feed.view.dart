78e43681208d5ed7e7f41a2774652910fb2c47d6
Achintya72 <achintya.agrawal.90@gmail.com>
2024-04-18 19:38:49 -0700

    Added End-of-Post message

--- lib/views/feed/feed.view.dart
+++ lib/views/feed/feed.view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
+import 'package:provider/provider.dart';
 import 'package:unbound/common/text_input.dart';
 import 'package:unbound/common/theme.dart';
 import 'package:unbound/model/feed.model.dart';
+import 'package:unbound/model/user.model.dart';
 import 'package:unbound/service/database.dart';
 import 'package:unbound/views/feed/post.widget.dart';
 
@@ -49,6 +51,7 @@ class _FeedState extends State<FeedPage> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
+    final String uid = Provider.of<AuthUser?>(context)?.uid ?? "";
     return FutureBuilder(
       future: future,
       builder: (context, snapshot) {
@@ -65,19 +68,29 @@ class _FeedState extends State<FeedPage> with SingleTickerProviderStateMixin {
                   padding: const EdgeInsets.all(20),
                   decoration: BoxDecoration(
                       color: white.shade50,
-                      boxShadow: scrollPos > 0 ? [BoxShadow(color: Color.fromRGBO(0, 0, 0, 0.25), blurRadius: 16)] : []),
+                      boxShadow: scrollPos > 0
+                          ? [
+                              BoxShadow(
+                                  color: Color.fromRGBO(0, 0, 0, 0.25),
+                                  blurRadius: 16)
+                            ]
+                          : []),
                   child: Column(
                     children: [
                       TextField(
                         decoration: textInputDecoration.copyWith(
-                            hintText: "Search", prefixIcon: Icon(Ionicons.search, size: 16, color: white.shade700)),
+                            hintText: "Search",
+                            prefixIcon: Icon(Ionicons.search,
+                                size: 16, color: white.shade700)),
                         style: Theme.of(context).textTheme.bodyLarge,
                       ),
                       const SizedBox(height: 12),
                       Container(
                           width: double.infinity,
                           padding: const EdgeInsets.all(2),
-                          decoration: BoxDecoration(color: white.shade400, borderRadius: BorderRadius.circular(10)),
+                          decoration: BoxDecoration(
+                              color: white.shade400,
+                              borderRadius: BorderRadius.circular(10)),
                           child: TabBar(
                               controller: controller,
                               indicatorSize: TabBarIndicatorSize.tab,
@@ -85,34 +98,74 @@ class _FeedState extends State<FeedPage> with SingleTickerProviderStateMixin {
                               indicator: BoxDecoration(
                                   color: white.shade50,
                                   borderRadius: BorderRadius.circular(8),
-                                  boxShadow: const [BoxShadow(color: Color.fromRGBO(0, 0, 0, 0.25), blurRadius: 8)]),
+                                  boxShadow: const [
+                                    BoxShadow(
+                                        color: Color.fromRGBO(0, 0, 0, 0.25),
+                                        blurRadius: 8)
+                                  ]),
                               tabs: [
                                 Tab(
                                   height: 30,
-                                  child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                                    Icon(currentIndex == 0 ? Ionicons.library : Ionicons.library_outline,
-                                        size: 14, color: currentIndex == 0 ? purple.shade400 : white.shade700),
-                                    const SizedBox(width: 8),
-                                    Text("Colleges", style: Theme.of(context).textTheme.bodySmall)
-                                  ]),
+                                  child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                            currentIndex == 0
+                                                ? Ionicons.library
+                                                : Ionicons.library_outline,
+                                            size: 14,
+                                            color: currentIndex == 0
+                                                ? purple.shade400
+                                                : white.shade700),
+                                        const SizedBox(width: 8),
+                                        Text("Colleges",
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodySmall)
+                                      ]),
                                 ),
                                 Tab(
                                   height: 30,
-                                  child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                                    Icon(currentIndex == 1 ? Ionicons.people : Ionicons.people_outline,
-                                        size: 14, color: currentIndex == 1 ? green.shade400 : white.shade700),
-                                    const SizedBox(width: 8),
-                                    Text("People", style: Theme.of(context).textTheme.bodySmall)
-                                  ]),
+                                  child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                            currentIndex == 1
+                                                ? Ionicons.people
+                                                : Ionicons.people_outline,
+                                            size: 14,
+                                            color: currentIndex == 1
+                                                ? green.shade400
+                                                : white.shade700),
+                                        const SizedBox(width: 8),
+                                        Text("People",
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodySmall)
+                                      ]),
                                 ),
                                 Tab(
                                   height: 30,
-                                  child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                                    Icon(currentIndex == 2 ? Ionicons.briefcase : Ionicons.briefcase_outline,
-                                        size: 14, color: currentIndex == 2 ? blue.shade600 : white.shade700),
-                                    const SizedBox(width: 8),
-                                    Text("Jobs", style: Theme.of(context).textTheme.bodySmall)
-                                  ]),
+                                  child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                            currentIndex == 2
+                                                ? Ionicons.briefcase
+                                                : Ionicons.briefcase_outline,
+                                            size: 14,
+                                            color: currentIndex == 2
+                                                ? blue.shade600
+                                                : white.shade700),
+                                        const SizedBox(width: 8),
+                                        Text("Jobs",
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodySmall)
+                                      ]),
                                 ),
                               ])),
                     ],
@@ -127,21 +180,120 @@ class _FeedState extends State<FeedPage> with SingleTickerProviderStateMixin {
                           controller: scrollController,
                           itemCount: collegeFeed.posts.length,
                           itemBuilder: (BuildContext context, int index) {
-                            return PostWidget(post: collegeFeed.posts[index]);
+                            return index != collegeFeed.posts.length - 1
+                                ? PostWidget(
+                                    post: collegeFeed.posts[index],
+                                    uid: uid,
+                                    type: "College")
+                                : Column(children: [
+                                    PostWidget(
+                                        post: collegeFeed.posts[index],
+                                        uid: uid,
+                                        type: "College"),
+                                    Container(
+                                      padding: const EdgeInsets.all(20),
+                                      child: Column(
+                                        children: [
+                                          Icon(Ionicons.book,
+                                              color: white.shade500, size: 36),
+                                          const SizedBox(height: 12),
+                                          Text("That's Enough For Today",
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .labelLarge!
+                                                  .copyWith(
+                                                      color: white.shade800)),
+                                          Text(
+                                              "Time to hit the books, the bed, or the buddies",
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .bodyMedium!
+                                                  .copyWith(
+                                                      color: white.shade800))
+                                        ],
+                                      ),
+                                    )
+                                  ]);
                           }),
                     if (studentFeed != null)
                       ListView.builder(
                           controller: scrollController,
                           itemCount: studentFeed.posts.length,
                           itemBuilder: (BuildContext context, int index) {
-                            return PostWidget(post: studentFeed.posts[index]);
+                            return index != studentFeed.posts.length - 1
+                                ? PostWidget(
+                                    post: studentFeed.posts[index],
+                                    uid: uid,
+                                    type: "User")
+                                : Column(children: [
+                                    PostWidget(
+                                        post: studentFeed.posts[index],
+                                        uid: uid,
+                                        type: "User"),
+                                    Container(
+                                      padding: const EdgeInsets.all(20),
+                                      child: Column(
+                                        children: [
+                                          Icon(Ionicons.book,
+                                              color: white.shade500, size: 36),
+                                          const SizedBox(height: 12),
+                                          Text("That's Enough For Today",
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .labelLarge!
+                                                  .copyWith(
+                                                      color: white.shade800)),
+                                          Text(
+                                              "Time to hit the books, the bed, or the buddies",
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .bodyMedium!
+                                                  .copyWith(
+                                                      color: white.shade800))
+                                        ],
+                                      ),
+                                    )
+                                  ]);
                           }),
                     if (internshipFeed != null)
                       ListView.builder(
                           controller: scrollController,
                           itemCount: internshipFeed.posts.length,
                           itemBuilder: (BuildContext context, int index) {
-                            return PostWidget(post: internshipFeed.posts[index]);
+                            return index != internshipFeed.posts.length - 1
+                                ? PostWidget(
+                                    post: internshipFeed.posts[index],
+                                    uid: uid,
+                                    type: "Internship")
+                                : Column(children: [
+                                    PostWidget(
+                                        post: internshipFeed.posts[index],
+                                        uid: uid,
+                                        type: "Internship"),
+                                    Container(
+                                      padding: const EdgeInsets.all(20),
+                                      child: Column(
+                                        children: [
+                                          Icon(Ionicons.book,
+                                              color: white.shade500, size: 36),
+                                          const SizedBox(height: 12),
+                                          Text("That's Enough For Today",
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .labelLarge!
+                                                  .copyWith(
+                                                      color: white.shade800)),
+                                          Text(
+                                              "Time to hit the books, the bed, or the buddies",
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .bodyMedium!
+                                                  .copyWith(
+                                                      color: white.shade800))
+                                        ],
+                                      ),
+                                    )
+                                  ]);
                           }),
                   ],
                 ))
