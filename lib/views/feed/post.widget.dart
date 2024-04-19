500cdfa401e416da4985de2d45baaa874c8b04dc
Achintya72 <achintya.agrawal.90@gmail.com>
2024-04-18 19:39:08 -0700

    Added liking, and displaying comments. Will add commenting later

--- lib/views/feed/post.widget.dart
+++ lib/views/feed/post.widget.dart
@@ -1,18 +1,38 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:provider/provider.dart';
 import 'package:unbound/common/theme.dart';
 import 'package:unbound/model/feed.model.dart';
 import 'package:unbound/model/user.model.dart';
+import 'package:unbound/service/database.dart';
 
-class PostWidget extends StatelessWidget {
+class PostWidget extends StatefulWidget {
   final Post post;
-  const PostWidget({super.key, required this.post});
+  final String uid;
+  final String type;
+  const PostWidget(
+      {super.key, required this.post, required this.uid, required this.type});
+
+  @override
+  State<PostWidget> createState() => _PostWidgetState();
+}
+
+class _PostWidgetState extends State<PostWidget> {
+  late bool liked;
+  late int likes;
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      liked = widget.post.likes.contains(widget.uid);
+      likes = widget.post.likes.length;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final String uid = Provider.of<AuthUser?>(context)?.uid ?? "";
     return Column(
       children: [
         Container(
@@ -24,7 +44,7 @@ class PostWidget extends StatelessWidget {
                 children: [
                   CircleAvatar(
                     backgroundColor: white.shade300,
-                    backgroundImage: NetworkImage(post.pfp),
+                    backgroundImage: NetworkImage(widget.post.pfp),
                     radius: 17.5,
                   ),
                   const SizedBox(width: 12),
@@ -32,26 +52,35 @@ class PostWidget extends StatelessWidget {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(post.author, style: Theme.of(context).textTheme.titleSmall),
-                        Text(timeToString(post.time),
-                            style: Theme.of(context).textTheme.bodySmall!.copyWith(color: white.shade700))
+                        Text(widget.post.author,
+                            style: Theme.of(context).textTheme.titleSmall),
+                        Text(timeToString(widget.post.time),
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodySmall!
+                                .copyWith(color: white.shade700))
                       ],
                     ),
                   ),
                   const SizedBox(width: 12),
-                  IconButton(onPressed: () {}, icon: Icon(Ionicons.person_add, size: 16, color: blue.shade600))
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(Ionicons.person_add,
+                          size: 16, color: blue.shade600))
                 ],
               ),
               const SizedBox(height: 12),
-              Text(post.text, style: Theme.of(context).textTheme.bodyLarge, textAlign: TextAlign.left)
+              Text(widget.post.text,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                  textAlign: TextAlign.left)
             ],
           ),
         ),
-        if (post.photo.isNotEmpty)
+        if (widget.post.photo.isNotEmpty)
           ClipRect(
             child: AspectRatio(
               aspectRatio: 16 / 9,
-              child: Image.network(post.photo, fit: BoxFit.cover),
+              child: Image.network(widget.post.photo, fit: BoxFit.cover),
             ),
           ),
         Container(
@@ -59,21 +88,103 @@ class PostWidget extends StatelessWidget {
           color: white.shade200,
           child: Row(children: [
             InkWell(
-              onTap: () {},
-              child: Icon(post.likes.contains(uid) ? Ionicons.heart : Ionicons.heart_outline,
-                  size: 18, color: post.likes.contains(uid) ? pink.shade300 : white.shade800),
+              onTap: () async {
+                if (liked) {
+                  await DatabaseService(uid: widget.uid)
+                      .removeLike(widget.post.uid, widget.type);
+                  setState(() {
+                    liked = false;
+                    likes--;
+                  });
+                } else {
+                  await DatabaseService(uid: widget.uid)
+                      .addLike(widget.post.uid, widget.type);
+                  setState(() {
+                    liked = true;
+                    likes++;
+                  });
+                }
+              },
+              child: Icon(liked ? Ionicons.heart : Ionicons.heart_outline,
+                  size: 18, color: liked ? pink.shade300 : white.shade800),
             ),
             const SizedBox(width: 6),
-            Text(post.likes.length.toString(), style: Theme.of(context).textTheme.labelLarge!.copyWith(color: white.shade800)),
+            Text(likes.toString(),
+                style: Theme.of(context)
+                    .textTheme
+                    .labelLarge!
+                    .copyWith(color: white.shade800)),
             const SizedBox(width: 12),
             InkWell(
-              onTap: () {},
-              child: Icon(Ionicons.chatbox_ellipses_outline, size: 18, color: white.shade800),
+              onTap: () {
+                showModalBottomSheet(
+                    context: context,
+                    builder: (BuildContext c) {
+                      return Container(
+                        height: 400,
+                        padding: const EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(20),
+                              topRight: Radius.circular(20)),
+                          color: white.shade50,
+                        ),
+                        child: ListView.separated(
+                          
+                          separatorBuilder: (context, index) => const SizedBox(height: 12),
+                          itemCount: widget.post.comments.length,
+                          itemBuilder: (context, index) {
+                            Comment comment = widget.post.comments[index];
+                            return Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                CircleAvatar(
+                                  backgroundColor: white.shade300,
+                                  backgroundImage: NetworkImage(comment.pfp),
+                                  radius: 17.5,
+                                ),
+                                const SizedBox(width: 12),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                                    children: [
+                                      Row(
+                                        crossAxisAlignment: CrossAxisAlignment.end,
+                                        children: [
+                                          Text(comment.author,
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .titleMedium),
+                                          const SizedBox(width: 12),
+                                          Text(timeToString(comment.time),
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .bodySmall!
+                                                  .copyWith(
+                                                      color: white.shade700))
+                                        ],
+                                      ),
+                                      Text(comment.text, style: Theme.of(context).textTheme.bodyMedium)
+                                    ],
+                                  ),
+                                )
+                              ],
+                            );
+                          },
+                        ),
+                      );
+                    });
+              },
+              child: Icon(Ionicons.chatbox_ellipses_outline,
+                  size: 18, color: white.shade800),
             ),
             const SizedBox(width: 6),
             Expanded(
-                child: Text(post.comments.length.toString(),
-                    style: Theme.of(context).textTheme.labelLarge!.copyWith(color: white.shade800))),
+                child: Text(widget.post.comments.length.toString(),
+                    style: Theme.of(context)
+                        .textTheme
+                        .labelLarge!
+                        .copyWith(color: white.shade800))),
           ]),
         )
       ],
