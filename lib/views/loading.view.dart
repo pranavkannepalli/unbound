fd56c6d22f6fe3d372d7cbed87a1d65943dbf973
PranavK226 <pranav.kannepalli@gmail.com>
2024-04-25 10:55:28 -0700

    debug

--- lib/views/loading.view.dart
+++ lib/views/loading.view.dart
@@ -6,6 +6,7 @@ class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: CircularProgressIndicator(),
